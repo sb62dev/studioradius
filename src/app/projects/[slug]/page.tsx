797b2dd5fae2c10fc242b6f104319527f95094bd
sb62dev <sb62dev@gmail.com>
2023-06-26 22:22:12 +0530

    Fixes

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,11 +1,51 @@
-import React from "react"; 
+import React from "react";
+import { createClient } from "contentful";
+import {
+    CONTENTFUL_SPACE_KEY,
+    CONTENTFUL_ACCESS_TOKEN,
+} from "../../../../public/api/constants";
 
-const ProjectDetails: React.FC = () => { 
-  return (
-    <main >
-        <h1> Projects Details </h1>
-    </main>
-  )
+if (!CONTENTFUL_SPACE_KEY || !CONTENTFUL_ACCESS_TOKEN) {
+    throw new Error("Contentful space key or access token is undefined.");
+}
+
+const client = createClient({
+    space: CONTENTFUL_SPACE_KEY,
+    accessToken: CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function generateStaticParams() {
+    const res = await client.getEntries({ content_type: "projects" });
+    const paths = res.items.map((item) => {
+        return {
+            slug: item.fields.slug
+        };
+    }); 
+    return  paths  
+}
+
+async function getPost(params: { slug: string }) {
+    const { items } = await client.getEntries({ 
+		content_type: "projects",
+		'fields.slug': params.slug
+	});  
+    return items; 
+}
+
+interface ProjectDetailsProps {
+    params: {
+        slug: string;
+    };
+}
+
+const ProjectDetails: React.FC<ProjectDetailsProps> = async ({ params }) => { 
+	const post = await getPost(params);
+	const postTitle = post?.[0]?.fields?.title; 
+    return (
+        <main>
+            <h1> Projects Details <br/> {postTitle as string}</h1>
+        </main>
+    );
 };
 
-export default ProjectDetails;
+export default ProjectDetails; 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,19 +4,17 @@ import Image from "next/image";
 import {getProjects} from "../../../public/api/contentfulAPI";   
 
 const Projects: React.FC = async () => {
-  const projectsData = await getProjects();  
-  const checkData = projectsData?.map((item) => item?.fields?.featureImage?.fields?.file);
-  console.log("check", checkData);
+  const projectsData = await getProjects();   
   const projectDataMap = projectsData?.length > 0 && projectsData?.map((item)=> (
-                            <>
-                                <div key={item?.sys?.id}>
-                                    <Image src={"https:" + item?.fields?.featureImage?.fields?.file?.url as string} width={"200"} height={"200"} alt={"sdf"}/> 
-                                    <Link href={`/projects/${item?.fields?.slug}`|| "#!"}> 
-                                        {item?.fields?.title as string}
-                                    </Link>
-                                </div> 
-                            </>
-                        )); 
+                    <>
+                        <div key={item?.sys?.id}>
+                            <Image src={"https:" + item?.fields?.featureImage?.fields?.file?.url as string} width={"200"} height={"200"} alt={"sdf"}/> 
+                            <Link href={`/projects/${item?.fields?.slug}`|| "#!"}> 
+                                {item?.fields?.title as string}
+                            </Link>
+                        </div> 
+                    </>
+                )); 
   return (
     <main >
         {projectDataMap} 
